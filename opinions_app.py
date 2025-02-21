170e77fe9546ba36741c264320060ce2c71c0a7c
Юрий Гапонов <GREATLORDorel@yandex.ru>
2025-02-21 03:54:45 +0300

    функция загрузки мнений из csv

--- opinions_app.py
+++ opinions_app.py
@@ -1,3 +1,7 @@
+import csv
+
+import click
+
 from datetime import datetime
 
 # Импортируется функция для выбора случайного значения:
@@ -5,6 +9,7 @@ from random import randrange
 
 # Импортируем функцию render_template():
 from flask import Flask, abort, flash, redirect, render_template, url_for
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
@@ -18,6 +23,7 @@ app.config['SECRET_KEY'] = '4vpoyg%&__$#!d&(w0p1o-o_l+1m*du*ef!8201xv35clhfd+='
 # Создаётся экземпляр класса SQLAlchemy и передаётся
 # в качестве параметра экземпляр приложения Flask:
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Opinion(db.Model):
@@ -26,6 +32,8 @@ class Opinion(db.Model):
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # Новое поле.
+    added_by = db.Column(db.String(64))
 
 
 # Класс формы опишите сразу после модели Opinion.
@@ -122,6 +130,27 @@ def page_not_found(error):
     # При ошибке 404 в качестве ответа вернётся страница, созданная
     # на основе шаблона 404.html, и код HTTP-ответа 404:
     return render_template('404.html'), 404
+
+
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    # Открываем файл:
+    with open('opinions.csv', encoding='utf-8') as f:
+        # Создаём итерируемый объект, который отображает каждую строку
+        # в качестве словаря с ключами из шапки файла:
+        reader = csv.DictReader(f)
+        # Для подсчёта строк добавляем счётчик:
+        counter = 0
+        for row in reader:
+            # Распакованный словарь используем
+            # для создания экземпляра модели Opinion:
+            opinion = Opinion(**row)
+            # Добавляем объект в сессию и коммитим:
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}')
 
 
 if __name__ == '__main__':
